583500cb63f1268570fe3fc60654e56f2b12fb97
Anon873-cyber <async2304@gmail.com>
2026-07-18 16:36:59 +0530

    feat: Refactor addToCart function to support wishlist and improve product order creation

--- Backend/src/controller/cart.controller.js
+++ Backend/src/controller/cart.controller.js
@@ -3,16 +3,21 @@ import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiError.js";
 import Product from "../model/product.js";
 
-const addToCart = async (req, res) => {
+const addToCartOrWishlist = async (req, res) => {
   try {
     const { productId, quantity } = req.body;
     const product = await Product.findById(productId);
     const userId = req.user._id;
+    const wishlist = req.query.wishlist === "true" ? true : false;
     if (!product) {
       throw new ApiError(404, "Product not found");
     }
-    const productOrder = await ProductOrder.create(productId, userId, quantity);
-    
+    const productOrder = await ProductOrder.create({
+      productId,
+      userId,
+      quantity,
+      wishlist,
+    });
 
     if (!productOrder) {
       throw new ApiError(500, "Failed to add product to cart");
@@ -25,4 +30,48 @@ const addToCart = async (req, res) => {
   }
 };
 
-export { addToCart };
+const editorDeleteCartItem = async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const userId = req.user._id;
+    const deleteParams = req.params.delete === "true" ? true : false;
+    const product = await Product.findOne(productId, userId);
+    if (!product) {
+      throw new ApiError(404, "Product not found");
+    }
+
+    if (deleteParams) {
+      const deletedItem = await ProductOrder.findOneAndDelete({
+        productId,
+        userId,
+      });
+    }
+    const updatedItem = await ProductOrder.findOneAndUpdate(
+      { productId, userId },
+      { $set: { quantity: req.body.quantity, wishlist: req.body.wishlist } },
+      { new: true },
+    );
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+};
+
+const getCartItems = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const cartItems = await ProductOrder.find({
+      userId,
+      wishlist: false,
+    }).populate("productId");
+    res
+      .status(200)
+      .json(
+        new ApiResponse(200, cartItems, "Cart items retrieved successfully"),
+      );
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+};
+
+export { addToCart, getCartItems, editorDeleteCartItem };
