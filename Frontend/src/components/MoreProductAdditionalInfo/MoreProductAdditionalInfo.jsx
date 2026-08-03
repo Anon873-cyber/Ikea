f7eb279c2adcf49242971d0ab9ea1386819a19d9
Anon873-cyber <async2304@gmail.com>
2026-08-03 19:25:53 +0530

    feat: Enhance MoreProductAdditionalInfo component layout and styling

--- Frontend/src/components/MoreProductAdditionalInfo/MoreProductAdditionalInfo.jsx
+++ Frontend/src/components/MoreProductAdditionalInfo/MoreProductAdditionalInfo.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Table from "../Ui/Table";
+import { Box, Check, ChevronDown, ChevronUp } from "lucide-react";
 
 function MoreProductAdditionalInfo() {
   return (
-    <div>MoreProductAdditionalInfo</div>
-  )
+    <div className="MoreProductAdditionalInfo w-6xl h-[546px] flex  bg-white rounded-xl flex-col shadow-xl gap-5">
+      <div className="flex items-center gap-2 p-5 font-[var(--font-heading)] text-[var(--color-heading)] font-medium text-xl">
+        <Box className="w-8 h-8" /> Product Details
+      </div>
+      <div className="flex flex-col max-w-[60%]">
+
+      </div>
+    </div>
+  );
 }
 
-export default MoreProductAdditionalInfo
+export default MoreProductAdditionalInfo;
