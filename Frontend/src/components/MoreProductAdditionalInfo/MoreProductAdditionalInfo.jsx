d7400531ff525727f3e366211315d431a1393d0b
Anon873-cyber <async2304@gmail.com>
2026-08-05 13:55:51 +0530

    feat: Implement product details fetching in MoreProductAdditionalInfo component

--- Frontend/src/components/MoreProductAdditionalInfo/MoreProductAdditionalInfo.jsx
+++ Frontend/src/components/MoreProductAdditionalInfo/MoreProductAdditionalInfo.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Table from "../Ui/Table";
 import { Box, Check, ChevronDown, ChevronUp } from "lucide-react";
+import axios from "axios";
 
-function MoreProductAdditionalInfo() {
+function MoreProductAdditionalInfo({ productId }) {
+  const lucideIcons = ["Box", "Check", "ChevronDown", "ChevronUp"];
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+     try {
+       const response = await axios.get(
+         `http://localhost:8000/api/v1/products/${productId}`,
+       );
+       
+     } catch (error) {
+      setError(error.message)
+     }
+    };
+
+    fetchProductDetails();
+  }, [productId]);
+
   return (
     <div className="MoreProductAdditionalInfo w-6xl h-[546px] flex  bg-white rounded-xl flex-col shadow-xl gap-5">
       <div className="flex items-center gap-2 p-5 font-[var(--font-heading)] text-[var(--color-heading)] font-medium text-xl">
         <Box className="w-8 h-8" /> Product Details
       </div>
-      <div className="flex flex-col max-w-[60%]">
-
-      </div>
+      <div className="flex flex-col max-w-[60%]"></div>
     </div>
   );
 }
