47a7dc936269af3d00696226d272f4cff4536824
Anon873-cyber <async2304@gmail.com>
2026-08-08 13:19:16 +0530

    added  wishlist and addtocart pages in auth layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,26 +7,29 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Contact from "./pages/Contact";
 import UserProfile from "./pages/UserProfile.jsx";
+import Wishlist from "./pages/Wishlist.jsx";
+import AddToCart from "./pages/AddToCart.jsx";
 
 import AuthLayout from "./components/Auth/Auth.jsx";
 
 function App() {
   return (
- 
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Shop />} />
-          <Route path="products" element={<Products />} />
-          <Route path="product/:productId" element={<ProductDetails />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<SignUp />} />
-          <Route path="contact" element={<Contact />} />
-          <Route element={<AuthLayout />}>
-            <Route path="profile" element={<UserProfile />} />
-          </Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Shop />} />
+        <Route path="products" element={<Products />} />
+        <Route path="product/:productId" element={<ProductDetails />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<SignUp />} />
+        <Route path="contact" element={<Contact />} />
+        <Route element={<AuthLayout />}>
+          <Route path="profile" element={<UserProfile />} />
+          <Route path="wishlist" element={<Wishlist />} />
+          <Route path="cart" element={<AddToCart />} />
+          <Route path="addtocart" element={<AddToCart />} />
         </Route>
-      </Routes>
-    
+      </Route>
+    </Routes>
   );
 }
 
