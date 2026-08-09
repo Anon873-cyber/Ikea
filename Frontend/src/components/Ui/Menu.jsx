5e0f36b2fc666d40148a54e5151925c16996c197
Anon873-cyber <async2304@gmail.com>
2026-08-09 19:50:43 +0530

    feat: add className prop to Menu component for customizable styling

--- Frontend/src/components/Ui/Menu.jsx
+++ Frontend/src/components/Ui/Menu.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Menu() {
+function Menu({className}) {
   return (
-    <section>
-      <div className="relative border top-10 border-gray-200 w-50 h-50 rounded shadow-sm">
+    <section className={className}>
+      <div className="relative bg-white border top-10 border-gray-200 w-50 h-50 rounded shadow-sm">
         <div className="absolute -top-2 left-5 w-4 h-4 rotate-45 border-l border-t border-gray-200 bg-white"></div>
       </div>
     </section>
