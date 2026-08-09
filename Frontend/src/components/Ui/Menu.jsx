5fd990ce1b2e7f968affb2bd744dd68c0f05a8e0
Anon873-cyber <async2304@gmail.com>
2026-08-09 18:15:59 +0530

    create menu

---
+++ Frontend/src/components/Ui/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <section>
+      <div className="relative border top-10 border-gray-200 w-50 h-50 rounded shadow-sm">
+        <div className="absolute -top-2 left-5 w-4 h-4 rotate-45 border-l border-t border-gray-200 bg-white"></div>
+      </div>
+    </section>
+  );
+}
+
+export default Menu;
