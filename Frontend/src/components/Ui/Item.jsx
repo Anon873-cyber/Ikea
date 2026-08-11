c88862cf7032638eedd6e22f5fba0faf9ac0510f
Anon873-cyber <async2304@gmail.com>
2026-08-11 15:37:31 +0530

    feat: create Item component for displaying product details

---
+++ Frontend/src/components/Ui/Item.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Trash2 as TrashIcon } from "lucide-react";
+
+
+function Item() {
+  return (
+    <section className="item">
+      <div className="container">
+        <div className="container">
+          <div className="image">
+            <img src="" alt="" srcset="" />
+          </div>
+          <div className="cointainer">
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+            <h2>{item.price}</h2>
+          </div>
+        </div>
+        <div className="container">
+
+          <button className="btn">
+            <TrashIcon size={20} />
+          </button>
+          <button>Move to Cart</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Item;
