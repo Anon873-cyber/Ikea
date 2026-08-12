666815f7aabcb1445f163ad6e2119effcd992d5a
Anon873-cyber <async2304@gmail.com>
2026-08-12 15:14:46 +0530

    refactor: enhance structure and styling of WishList component

--- Frontend/src/components/WishList/WishList.jsx
+++ Frontend/src/components/WishList/WishList.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import useFetch from "../../hooks/useFetch";
 import NoWishList from "./NoWishlist";
 import Item from "../Ui/Item";
+import { useState } from "react";
 
 function WishList() {
   const { data, isLoading, error } = useFetch("/cart?wishlist=true");
+  const [TotalPrice, setTotalPrice] = useState(0);
 
-  console.log(data,"data response");
+  console.log(data, "data response");
   if (isLoading) {
     return <div>Loading ....</div>;
   }
@@ -18,19 +20,28 @@ function WishList() {
   if (!data.length) {
     return <NoWishList />;
   }
- 
-  return (
-    <section className="Wish-list-items">
-      <div className="top">
-        <h1 className="[font-family:var(--font-primary)]">My Wishlist</h1>
-        <p>Save products you love and come back to them anytime</p>
-      </div>
-      {data.map((item) => (
-        <div>
 
-        <Item item={item.productId} />;
+  return (
+    <section className="Wish-list-items ">
+      <div className="container flex flex-col m-auto max-w-6xl  gap-15 py-10">
+        <div className="top">
+          <h1 className="text-4xl font-medium  font-[var(--font-primary)] ">
+            My Wishlist
+          </h1>
+          <p className="text-gray-600  font-[var(--font-body)]">
+            Save products you love and come back to them anytime
+          </p>
         </div>
-      ))}
+
+        {data.map((item) => (
+          <div>
+            <Item item={item.productId} />
+          </div>
+        ))}
+      </div>
+      
+
+
     </section>
   );
 }
