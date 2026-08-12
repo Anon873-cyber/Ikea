69f5b861087428f6a39a163b5e2283319b7b1d3d
Anon873-cyber <async2304@gmail.com>
2026-08-12 15:05:31 +0530

    refactor: improve structure and styling of Item component

--- Frontend/src/components/Ui/Item.jsx
+++ Frontend/src/components/Ui/Item.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import { Trash2 as TrashIcon } from "lucide-react";
 
-
-function Item({item}) {
-  console.log("item component")
-  console.log(item,"item ")
+function Item({ item }) {
+  console.log("item component");
+  console.log(item, "item ");
   return (
     <section className="item">
-      <div className="container">
-        <div className="container">
-          <div className="image">
-            <img src="" alt="" srcset="" />
+      <div className="mx-auto flex max-w-6xl gap-6 border-t border-gray-200  p-6">
+        {/* Product */}
+        <div className="flex flex-1 gap-6">
+          <div className="w-40 rounded-xl shrink-0">
+            <img
+              src={item.images[0].url}
+              alt={item.productName}
+              className="h-35 w-35 rounded-lg object-cover"
+            />
           </div>
-          <div className="cointainer">
-            <h1>{item.productName}</h1>
-            <p>{item.productDescription}</p>
-            <h2>{item.price}</h2>
+
+          <div className="flex flex-col justify-center">
+            <h1 className="text-xl font-semibold">{item.productName}</h1>
+
+            <p className="mt-2 text-gray-600">{item.productDescription}</p>
+
+            <h2 className="mt-3 text-lg font-bold">₹{item.price}</h2>
           </div>
         </div>
-        <div className="container">
 
-          <button className="btn">
+        {/* Actions */}
+        <div className="flex items-center gap-3">
+          <button className="rounded-lg p-2 hover:bg-red-100">
             <TrashIcon size={20} />
           </button>
-          <button>Move to Cart</button>
+
+          <button className="rounded-lg bg-[var(--color-primary)] px-4 py-2 text-white hover:bg-gray-800">
+            Move to Cart
+          </button>
         </div>
       </div>
     </section>
