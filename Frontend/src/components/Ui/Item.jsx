23a15caa285a18f07ae725610f2e253352418778
Anon873-cyber <async2304@gmail.com>
2026-08-12 11:11:38 +0530

    added key  of the props for rendering

--- Frontend/src/components/Ui/Item.jsx
+++ Frontend/src/components/Ui/Item.jsx
@@ -3,6 +3,8 @@ import { Trash2 as TrashIcon } from "lucide-react";
 
 
 function Item({item}) {
+  console.log("item component")
+  console.log(item,"item ")
   return (
     <section className="item">
       <div className="container">
@@ -11,8 +13,8 @@ function Item({item}) {
             <img src="" alt="" srcset="" />
           </div>
           <div className="cointainer">
-            <h1>{item.title}</h1>
-            <p>{item.description}</p>
+            <h1>{item.productName}</h1>
+            <p>{item.productDescription}</p>
             <h2>{item.price}</h2>
           </div>
         </div>
