f6cad550918d74ed71e64c720dffabb92448fefb
Anon873-cyber <async2304@gmail.com>
2026-08-12 10:28:42 +0530

    feat: enhance WishList component to display items and loading/error states

--- Frontend/src/components/WishList/WishList.jsx
+++ Frontend/src/components/WishList/WishList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useFetch from "../../hooks/useFetch";
 import NoWishList from "./NoWishlist";
+import Item from "../Ui/Item";
 
 function WishList() {
   const { data, isLoading, error } = useFetch("/cart");
@@ -16,8 +17,18 @@ function WishList() {
   if (!data.length) {
     return <NoWishList />;
   }
- console.log(data,"data data")
-  return <div>Wishlist</div>;
+  console.log(data, "data data");
+  return (
+    <section className="Wish-list-items">
+      <div className="top">
+        <h1 className="[font-family:var(--font-primary)]">My Wishlist</h1>
+        <p>Save products you love and come back to them anytime</p>
+      </div>
+      {data.data.map((item) => {
+        <Item item={item} />;
+      })}
+    </section>
+  );
 }
 
 export default WishList;
