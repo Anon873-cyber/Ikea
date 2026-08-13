b474dbb8efd35dd52bb3b20c42247dfee8117a94
Anon873-cyber <async2304@gmail.com>
2026-08-13 13:03:01 +0530

    refactor: simplify fetchData function structure and enhance return value in useFetch hook

--- Frontend/src/hooks/useFetch.js
+++ Frontend/src/hooks/useFetch.js
@@ -1,30 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 //import axios from 'axios';
-import axios from '../api/axios.js'
+import axios from "../api/axios.js";
 
-const useFetch = (url='/cart', options = {},method = 'get') => {
+const useFetch = (url = "/cart", options = {}, method = "get") => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios[method](url, options);
+      console.log(response.data);
+      setData(response.data.data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await axios[method](url, options);
-        console.log(response.data)
-        setData(response.data.data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        
-        setIsLoading(false);
-      }
-    };
-
     fetchData();
-  }, [url, JSON.stringify(options), method]); 
-  return { data, isLoading, error };
+  }, [url, JSON.stringify(options), method]);
+  return { data, isLoading, error, refetch: fetchData };
 };
 
-export default useFetch;   
+export default useFetch;
